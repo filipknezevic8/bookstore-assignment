eed38a560df743533f6bdf1f844c6d2fd312e54a
Filip Knezevic <filipknezevic369@gmail.com>
2026-02-12 01:59:07 +0100

    Definisana konfiguracija šeme baze podataka kroz Fluent API

--- BookstoreApplication/BookstoreApplication/Models/AppDbContext.cs
+++ BookstoreApplication/BookstoreApplication/Models/AppDbContext.cs
@@ -11,5 +11,37 @@ namespace BookstoreApplication.Models
         public DbSet<Publisher> Publishers { get; set; }
         public DbSet<Award> Awards { get; set; }
         public DbSet<AuthorAward> AuthorAwards { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<AuthorAward>(entity =>
+            {
+                entity.ToTable("AuthorAwardBridge");
+            });
+
+            modelBuilder.Entity<Author>()
+                .Property(a => a.DateOfBirth)
+                .HasColumnName("Birthday");
+
+            modelBuilder.Entity<AuthorAward>()
+                .HasOne(aa => aa.Author)
+                .WithMany(a => a.AuthorAwards)
+                .HasForeignKey(aa => aa.AuthorId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<AuthorAward>()
+                .HasOne(aa => aa.Award)
+                .WithMany(aw => aw.AuthorAwards)
+                .HasForeignKey(aa => aa.AwardId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Book>()
+                .HasOne(b => b.Publisher)
+                .WithMany()
+                .HasForeignKey(b => b.PublisherId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
